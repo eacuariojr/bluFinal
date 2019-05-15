4971b4d62a4b995194a2fe2b4aa7d992e8f1ccce
ErwinA <acuario.erwin00@gmail.com>
2019-05-15 13:47:00 -0700

    Created new project. Ignore XML changes
    yep, thats what I did!

---
+++ README.md
@@ -0,0 +1,11 @@
+# bluFinal
+Final project for Intro to Java
+
+
+List any ideas here! 
+
+A program that automatically creates a work schedule for a business
+
+A rouge-like game where player starts over if they lose/ implement score board from lab 14
+
+
---
+++ src/BluFinal.java
@@ -0,0 +1,13 @@
+/*
+    Erwin Aquario, Victor Gallardo and Kevin Raya
+    May 15, 2019
+    BluFinal.java
+    Dependencies: none
+    Credit: none
+*/
+//----------------------------------------------------------------------------------------------------------------------
+import java.util.Scanner; //import Scanner class
+public class BluFinal
+{
+
+}
