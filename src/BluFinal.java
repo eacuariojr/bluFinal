a93ab0bd90db428a863ef95fa09bc304021dfe9b
vroninG <gallardovctr@gmail.com>
2019-05-16 13:54:55 -0700

    Did stuff
    Okay man

--- src/BluFinal.java
+++ src/BluFinal.java
@@ -7,19 +7,35 @@
 */
 //----------------------------------------------------------------------------------------------------------------------
 import java.util.Scanner;
-public class BluFinal
-{
-    public static void main(String[] args)
-    {
-        //System.out.print("Hello final project!");
-    }
 
-    private static void mainMenu()
+public class BluFinal //start BluFinal class
+{
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) //start main method
+    {
+        printMenu();
+        int choice = getChoice();
+
+        switch(choice) //start switch statement
+        {
+
+        } //end switch statement
+    } //end main method
+
+    private static void printMenu() //start printMenu method
     {
         System.out.println("*****MAIN MENU*****");
         System.out.println("1.New Game");
         System.out.println("2.Load Game");
         System.out.println("3.Score Board");
         System.out.println("4.Quit");
-    }
-}
+    } //end printMenu method
+
+    private static int getChoice() //start getChoice method
+    {
+        System.out.print("\nEnter your choice: ");
+        int choice = input.nextInt();
+        return choice;
+    } //end getChoice method
+} //end BluFinal class
