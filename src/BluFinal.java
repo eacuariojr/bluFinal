44834f9b3161ffc2836ca982c2caa30bc69164b3
vroninG <gallardovctr@gmail.com>
2019-05-18 17:23:38 -0700

    Finshed leaderboard and quitMenu methods in main class

--- src/BluFinal.java
+++ src/BluFinal.java
@@ -2,11 +2,15 @@
     Erwin Aquario, Victor Gallardo and Kevin Raya
     May 15, 2019
     BluFinal.java
-    Dependencies: none
+    Dependencies: Character.java, Game.java, Party.java
     Credit: none
 */
 //----------------------------------------------------------------------------------------------------------------------
+//import classes
+import java.io.File;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 
 public class BluFinal //start BluFinal class
 {
@@ -14,24 +18,55 @@ public class BluFinal //start BluFinal class
 
     public static void main(String[] args) //start main method
     {
-        printMenu();
-        int choice = getChoice();
+        int choice; //delcare int choice to store user choice
+        boolean validCommand = true; //validCommand set to true if command is int
 
-        switch(choice) //start switch statement
+        do
         {
-            case 1:
-                System.out.println("Hello!");
-                break;
-        } //end switch statement
+            validCommand = true;
+
+            try //attempt this block of code
+            {
+                do {
+                    printMenu(); //printMenu method prints menu
+                    choice = getChoice(); //initialize choice with getChoice
+
+                    switch (choice) //start switch statement
+                    {
+                        case 1:
+                            System.out.println("Test 1");
+                            break;
+                        case 2:
+                            System.out.println("Test 2");
+                            break;
+                        case 3:
+                            leaderboard();
+                            break;
+                        case 4:
+                            quitGame();
+                            break;
+                        default:
+                            System.out.println("Invalid command. Try again.");
+                            break;
+                    } //end switch statement
+                } while(choice != 4);
+            }
+            catch(InputMismatchException error) //throw exception if input does not match
+            {
+                input.nextLine(); //clear the rest of the line
+                validCommand = false; //set to false if exception is thrown
+                System.out.println("Invalid command. Try again.");
+            }
+        } while(!validCommand); //do while valid command is not true
     } //end main method
 
     private static void printMenu() //start printMenu method
     {
-        System.out.println("*****MAIN MENU*****");
+        System.out.println("\n~~~~~MAIN MENU~~~~~");
         System.out.println("1.New Game");
         System.out.println("2.Load Game");
-        System.out.println("3.Score Board");
-        System.out.println("4.Quit");
+        System.out.println("3.Scoreboard");
+        System.out.println("4.Quit Game");
     } //end printMenu method
 
     private static int getChoice() //start getChoice method
@@ -40,4 +75,39 @@ public class BluFinal //start BluFinal class
         int choice = input.nextInt();
         return choice;
     } //end getChoice method
+
+    private static void leaderboard() //start leaderboard method
+    {
+        //open file
+        String filename = "Scoreboard.txt"; //create String variable with file name
+        File myFile = new File(filename); //finds the relative path to Scoreboard.txt
+
+        //read and print data from Scoreboard.txt
+        String line;
+        System.out.println("~~~~~PRINTING LEADERBOARD~~~~~");
+
+        try //try finding and printing the file
+        {
+            Scanner fileStream = new Scanner(myFile); //Scanner reads Scoreboard.txt file instead of user input
+
+            while(fileStream.hasNext())
+            {
+                String username = fileStream.next();
+                fileStream.next();
+                int score = fileStream.nextInt();
+
+                System.out.printf("%s -- %d\n", username, score);
+            }
+        }
+        catch(FileNotFoundException error) //if file cannot be found, throw a FileNotFoundExcpetion
+        {
+            System.out.println(error);
+        }
+    } //end leaderoard method
+
+    private static void quitGame() //start quitGame method
+    {
+        System.out.print("\n~~~~~End Game~~~~~\n");
+        System.exit(0); //terminates program
+    } //end quitGame method
 } //end BluFinal class
--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,12 @@
-public class Character
+/*
+    Erwin Aquario, Victor Gallardo and Kevin Raya
+    May 15, 2019
+    Character.java
+    Dependencies: none
+    Credit: none
+*/
+//----------------------------------------------------------------------------------------------------------------------
+public class Character //start Character class
 {
-}
+
+} //end class
